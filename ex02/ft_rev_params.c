2da4c222e7feac1a9fb29b8001c16c80bb2adafa
Lisa Jongmans <ljongman@e-r9-p4.s19.be>
2019-07-22 09:53:32 +0200

    ft_rev_params

---
+++ ex02/ft_rev_params.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ljongman <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/22 09:50:10 by ljongman          #+#    #+#             */
+/*   Updated: 2019/07/22 09:52:43 by ljongman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <string.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	while ((argc -1) >= 1)
+	{
+		ft_putstr(argv[argc - 1]);
+		ft_putchar('\n');
+		argc--;
+	}
+	return (0);
+}
